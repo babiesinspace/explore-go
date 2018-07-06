71b84eac12a4a6d0b9ea3a137279b3a592a542dd
babiesinspace <allie.g.cooper@gmail.com>
2018-07-06 10:42:55 -0400

    file stored in wrong place

--- api/scramble.js
+++
@@ -1,7 +0,0 @@
-function scrambler(string) {
-	let characters = string.split("")
-	for (var i = 0; i < characters.length; i++) {
-		var span = $('<span />').addClass('hidden').text(characters[i]);
-		$('body').append(span)
-	}
-}
